0b24755f981b6630f8f8d3bddeb258d96187ad58
pochkas <lavsvetlav@gmail.com>
2023-01-28 18:38:26 +0300

    add model
    Signed-off-by: pochkas <lavsvetlav@gmail.com>

---
+++ src/main/java/model/Expense.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class Expense {
+
+    private Long id;
+
+    private ExpenseCategory categories;
+
+    private double price;
+
+    private LocalDateTime date;
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/model/ExpenseCategory.java
@@ -0,0 +1,19 @@
+package model;
+
+public enum ExpenseCategory {
+
+    SHOP,
+    CLOTHES,
+    FOOD,
+    CAFE,
+    APARTMENT,
+    INVESTMENT,
+    TECHNICS,
+    AUTO,
+    TRAVEL,
+    TRANSPORT,
+    OTHER
+
+
+
+}
