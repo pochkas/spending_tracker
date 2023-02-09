de18bdecffe9582fee303470d79f84013ce1fc30
Svetlana <lavsvetlav@gmail.com>
2023-02-09 23:41:13 +0300

    correct dependency

--- src/main/java/org/example/ExpenseTrackerApplication.java
+++ src/main/java/org/example/ExpenseTrackerApplication.java
@@ -1,21 +1,9 @@
 package org.example;
 
-import org.example.model.Expense;
-import org.example.model.ExpenseCategory;
-import org.example.converters.CategoriesAndPrice;
-import org.example.repository.ExpenseRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.example.model.ExpenseCategory.*;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
